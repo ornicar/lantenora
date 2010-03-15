051c0d776ad30f4452768252df978e036ed83b64
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-15 01:05:20 +0100

    fixed admin js

--- public_html/js/admin.js
+++ public_html/js/admin.js
@@ -1,29 +1,24 @@
 (function($) {
-  
-$.dm.ctrl.add({
 
-  init : function(options)
+$(function()
+{
+  
+  if ($tagsText = $('#image_admin_form_tags_text').orNot())
   {
-    this.options = options;
-
-    if ($tagsText = $('#image_admin_form_tags_text').orNot())
-		{
-			$.ajax({
-				dataType: 'json',
-				url: $.dm.ctrl.getHref('+/tag/json'),
-				success: function(data)
-				{
-					$tagsText.autocomplete(data, {
-						multiple: true,
-						multipleSeparator: ', ',
-						matchContains: true,
-					  selectFirst: false,
-					});
-				}
-			})
-		}
-  },
-  
+    $.ajax({
+      dataType: 'json',
+      url: $.dm.ctrl.getHref('+/tag/json'),
+      success: function(data)
+      {
+        $tagsText.autocomplete(data, {
+          multiple: true,
+          multipleSeparator: ', ',
+          matchContains: true,
+          selectFirst: false
+        });
+      }
+    })
+  }
 });
 
 })(jQuery);
