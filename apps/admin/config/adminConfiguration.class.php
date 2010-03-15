f2bb98190cec56da8133fd4ebc03f00de9f9f5df
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-15 01:15:11 +0100

    fixed admin boxes

--- apps/admin/config/adminConfiguration.class.php
+++ apps/admin/config/adminConfiguration.class.php
@@ -6,5 +6,22 @@ class adminConfiguration extends dmAdminApplicationConfiguration
 {
   public function configure()
   {
+    $this->dispatcher->connect('dm.admin_homepage.filter_windows', array($this, 'listenToFilterWindowsEvent'));
+  }
+
+  public function listenToFilterWindowsEvent(sfEvent $event, array $windows)
+  {
+    $windows[1] = array(
+      'visitChart' => $windows[1]['visitChart'],
+      'commentaires' => array($this, 'renderCommentaires'),
+      'logChart' => $windows[1]['logChart'],
+    );
+
+    return $windows;
+  }
+
+  public function renderCommentaires(dmHelper $helper)
+  {
+    return $helper->renderComponent('commentaire', 'last');
   }
 }
