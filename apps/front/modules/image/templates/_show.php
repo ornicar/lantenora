1d28266943b14272f773198b52853afdf329c78b
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-26 00:37:04 +0100

    updated some helper calls

--- apps/front/modules/image/templates/_show.php
+++ apps/front/modules/image/templates/_show.php
@@ -4,23 +4,23 @@
  * Vars : $image
  */
 
-echo £o('div#diapo_image.image.big', array('json' => array(
+echo _open('div#diapo_image.image.big', array('json' => array(
   'diapo_delay' => dmConfig::get('diapo_delay', 5) * 1000,
   'preload_url' => isset($preloadImage) ? $preloadImage : null
 )));
 
-echo £('div#image_help',
-  £('p', £('strong', 'F11').'plein écran').
-  £('p', £('strong', 'Echap').'quitter').
-  £('p', £('strong', '←').'précédent').
-  £('p', £('strong', '→').'suivant').
-  £('p', £('strong', 'Espace').'diaporama')
+echo _tag('div#image_help',
+  _tag('p', _tag('strong', 'F11').'plein écran').
+  _tag('p', _tag('strong', 'Echap').'quitter').
+  _tag('p', _tag('strong', '←').'précédent').
+  _tag('p', _tag('strong', '→').'suivant').
+  _tag('p', _tag('strong', 'Espace').'diaporama')
 );
 
-$media = £media($image->Media)
+$media = _media($image->Media)
 ->size($maxSize)
 ->method('scale')
-->overlay(£media('overlay/logo.png'), 'bottom-right');
+->overlay(_media('overlay/logo.png'), 'bottom-right');
 
 $realSize = $media->getRealSize();
 $htmlSize = $sf_user->getBestSize($realSize);
@@ -30,79 +30,79 @@ if ($realSize != $htmlSize)
   $media->htmlSize($htmlSize);
 }
 
-echo £('div#full_image style="width: '.$htmlSize[0].'px; height: '.$htmlSize[1].'px;"',
+echo _tag('div#full_image style="width: '.$htmlSize[0].'px; height: '.$htmlSize[1].'px;"',
   $media->set('.full_image.full_size')
 );
 
-echo £o('div.infos.screen_center');
+echo _open('div.infos.screen_center');
 
-echo £('h1.t_image', $image->nom);
+echo _tag('h1.t_image', $image->nom);
 
-echo £o('div.infos_box');
+echo _open('div.infos_box');
 
-echo £("div.top",
-  £("div", £link($image->auteurPage)).
-  £("div.sep", "|").
-  £("div", $image->date ? $image->date : format_date($image->createdAt)).
-  £("div.sep", "|").
-  £("div", £link("#commentaires")->text(plural("commentaire", count($image->Commentaires))))
+echo _tag("div.top",
+  _tag("div", _link($image->auteurPage)).
+  _tag("div.sep", "|").
+  _tag("div", $image->date ? $image->date : format_date($image->createdAt)).
+  _tag("div.sep", "|").
+  _tag("div", _link("#commentaires")->text(plural("commentaire", count($image->Commentaires))))
 );
 
-echo £o("ul.image_tags.clearfix");
+echo _open("ul.image_tags.clearfix");
 
 foreach($image->Tags as $tag)
 {
-  echo £("li.element", £link($tag));
+  echo _tag("li.element", _link($tag));
 }
 
-echo £c("ul"), £c('div');
+echo _close("ul"), _close('div');
 
-echo £o("div.description_technique.clearfix");
+echo _open("div.description_technique.clearfix");
 
-echo £('div#description', markdown($image->description));
+echo _tag('div#description', markdown($image->description));
 
 if ($image->Commentaires->count())
 {
-  echo £o('div.commentaire.list_by_image.list');
+  echo _open('div.commentaire.list_by_image.list');
 
-    echo £o('ul.elements');
+    echo _open('ul.elements');
   
     foreach ($image->Commentaires as $commentaire)
     {
-      echo £o('li.element');
+      echo _open('li.element');
       
-        echo £("div.clearfix",
-          £media("gravatar.jpg")->set(".fleft.imgleft").
-          £("p.date", format_date($commentaire->createdAt)).
-          £("p.auteur", auto_link_text(escape($commentaire->auteur)))
+        echo _tag("div.clearfix",
+          _media("gravatar.jpg")->set(".fleft.imgleft").
+          _tag("p.date", format_date($commentaire->createdAt)).
+          _tag("p.auteur", auto_link_text(escape($commentaire->auteur)))
         ).
-        £("div.texte", nl2br(auto_link_text(escape($commentaire->texte))));
+        _tag("div.texte", nl2br(auto_link_text(escape($commentaire->texte))));
         
-      echo £c('li');
+      echo _close('li');
     }
   
-    echo £c('ul');
+    echo _close('ul');
   
-  echo £c('div');
+  echo _close('div');
 }
 
-echo £c("div");
+echo _close("div");
 
-echo £c('div');
+echo _close('div');
 
-if($prevPage) echo £link($prevPage)->textTitle($prevPage->name)->set('#prev_image');
+if($prevPage) echo _link($prevPage)->textTitle($prevPage->name)->set('#prev_image');
 
-if($nextPage) echo £link($nextPage)->textTitle($nextPage->name)->set('#next_image');
+if($nextPage) echo _link($nextPage)->textTitle($nextPage->name)->set('#next_image');
 
 if ($back && !$back->getNode()->isRoot())
 {
-  echo £link($back)->textTitle('Retour à '.$back->name)->set('#image_back');
+  echo _link($back)->textTitle('Retour à '.$back->name)->set('#image_back');
 }
 else
 {
-  echo £link()->textTitle('Retour à l\'acceuil')->set('#image_back');
+  echo _link()->textTitle('Retour à l\'acceuil')->set('#image_back');
 }
 
-if($nextPage) echo £('a#image_play', 'Diaporama');
+if($nextPage) echo _tag('a#image_play', 'Diaporama');
 
-echo £c('div');
+echo _close('div');
