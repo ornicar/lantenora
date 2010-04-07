41ea9b247b1f1edf54f75a7c1108992b896ce202
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-07 19:54:59 +0200

    changed tag cloud size

--- apps/front/modules/dmTag/templates/_cloud.php
+++ apps/front/modules/dmTag/templates/_cloud.php
@@ -5,7 +5,7 @@ echo _open('div.clearfix');
 foreach($tags as $tag)
 {
   echo _link($tag)->text($tag->get('name'))
-  ->set('style', 'font-size: '.round(3/4+$tag->get('nb_images')/15, 2).'em');
+  ->set('style', 'font-size: '.round(4/5+$tag->get('nb_images')/30, 2).'em');
 }
 
 echo _close('div');
