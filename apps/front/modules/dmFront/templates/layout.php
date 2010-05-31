e33dda5219913eb65f0d96d61b6f6a4a1ab98631
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-05-31 20:04:57 +0200

    Fix body background

--- apps/front/modules/dmFront/templates/layout.php
+++ apps/front/modules/dmFront/templates/layout.php
@@ -9,7 +9,8 @@ $helper->renderHtmlTag(),
   "\n<head>\n",
     $helper->renderHead(),
     sprintf(
-      '<style type="text/css">body {background: #000 url(/theme/images/background/%d.jpg) top left no-repeat;}</style>',
+      '<style type="text/css">body {background: #000 url(%s/theme/images/background/%d.jpg) top left no-repeat;}</style>',
+      $sf_request->getAbsoluteUrlRoot(),
       $helper->getOption('background_number', 1)
     ),
   "\n</head>\n",
